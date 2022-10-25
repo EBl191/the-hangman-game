6b1ebc6f7af28e72ce0f8a998321fa151d43e2ec
Eduardo Blanco <rafael.blanco1993@gmail.com>
2022-10-25 12:47:14 -0500

    main files uploaded

---
+++ src/App.tsx
@@ -0,0 +1,110 @@
+import { HangImage } from './components/HangImage'
+import { letters} from './helpers/letters'
+
+import './App.css'
+import { useEffect, useState } from 'react'
+import { getRandomWord } from './helpers/getRandomWord';
+
+function App() {
+
+  const [ word, setWord ] = useState(getRandomWord());
+  const [ hiddenWord, setHiddenWord ] = useState( '_ '.repeat(word.length));
+
+  const [ attempts, setAttempts ] = useState(0);
+  const [ lose, setLose ] = useState( false );
+  const [ won, setWon ] = useState( false );
+
+  useEffect( () => {
+    if (attempts >= 9) {
+      setLose( true );
+    }
+  }, [ attempts ]);
+
+  useEffect( ()=> {
+
+    const currentHiddenWord = hiddenWord.split(' ').join('');
+    if ( currentHiddenWord === word ) {
+      setWon( true );
+    }
+
+  }, [hiddenWord])
+
+  const checkLetter = (letter: string) => {
+
+    if (lose) return;
+    if (won) return;
+        
+    if ( !word.includes(letter)) {
+      setAttempts( Math.min(attempts + 1, 9));
+      return;
+    }
+
+    const hiddenWordArray = hiddenWord.split(' ');
+
+    for( let i = 0; i < word.length; i++) {
+      if ( word[i] === letter ) {
+        hiddenWordArray[i] = letter;
+      }
+    }
+    
+    setHiddenWord(hiddenWordArray.join(' '));
+  }
+
+  const newGame = () => {
+    const newWord = getRandomWord();
+
+    setWord(newWord);
+    setHiddenWord('_ '.repeat( newWord.length));
+    setAttempts(0);
+    setLose(false);
+    setWon(false);
+  }
+
+  return (
+    <div className="App">
+
+    {/* Images */}
+    <HangImage imageNumber={ attempts }/>
+
+    {/* Hidden word */}
+    <h3>{hiddenWord}</h3>
+
+    {/* Counter: tries */}
+    <h3>Intentos = {attempts} </h3>
+
+     {/* Message: fail */}
+     {
+       ( lose ) 
+       ? <h2>Perdió... La palabra correcta era: { word }</h2>
+       : ''
+     }
+
+     {/* Message: Victory */}
+     {
+       ( won ) 
+       ? <h2>Felicidades, usted ganó</h2>
+       : ''
+     }
+
+
+    {/* Buttons */}
+    {
+      letters.map((letter) => (
+        <button
+        onClick={ () => checkLetter(letter)}
+         key={letter}>
+          {letter}
+        </button>
+      ))
+    }
+    
+    <br /><br />
+    <button onClick={ newGame }>Jugar de nuevo</button>
+
+
+    </div>
+  )
+     
+}
+
+export default App
